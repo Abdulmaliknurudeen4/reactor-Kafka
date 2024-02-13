786bfb0eac0a38a8f68a133e936ed80020a740ff
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-02-13 08:28:29 +0100

    02 - Consuming from Multiple Sources

--- src/main/java/com/nexusforge/reactivekafka/sec01/Lec02KafkaConsumer.java
+++ src/main/java/com/nexusforge/reactivekafka/sec01/Lec02KafkaConsumer.java
@@ -7,30 +7,31 @@ import org.slf4j.LoggerFactory;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 
-import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
-public class Lec01KafkaConsumer {
+public class Lec02KafkaConsumer {
 
-    private static final Logger log = LoggerFactory.getLogger(Lec01KafkaConsumer.class);
+    private static final Logger log = LoggerFactory.getLogger(Lec02KafkaConsumer.class);
+
     public static void main(String[] args) {
         var consumerConfig = Map.<String, Object>of(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                ConsumerConfig.GROUP_ID_CONFIG, "demo-group124",
+                ConsumerConfig.GROUP_ID_CONFIG, "inventory-service-group",
                 ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
                 ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1",
                 ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false
         );
 
         var options = ReceiverOptions.create(consumerConfig)
-                .subscription(List.of("order-events"));
+                .subscription(Pattern.compile("order.*"));
 
         // uses - non deamon thread...
         KafkaReceiver.create(options).receive()
-                .doOnNext(r -> log.info("key: {}, value:{}", r.key(), r.value()))
+                .doOnNext(r -> log.info("topic: {}, key: {}, value:{}", r.topic(), r.key(), r.value()))
                 .doOnNext(r -> r.receiverOffset().acknowledge())
                 // commit interval.ms = 5000 ms. Ackowledge intervals
                 .subscribe();
