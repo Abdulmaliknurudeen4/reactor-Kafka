4b6ef6d0a121c950f025a2644d49ba09619a0167
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-02-13 07:51:38 +0100

    01 - Kafka Setup with Configuration

---
+++ src/test/java/com/nexusforge/reactivekafka/ReactiveKafkaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nexusforge.reactivekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+
+@SpringBootTest
+class ReactiveKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
