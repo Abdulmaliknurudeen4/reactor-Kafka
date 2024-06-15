88bf17765d56816e076b455a90bf48ae4980031b
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-15 23:09:38 +0100

    09 - Batch Auto Recieve Acknowledge / using flatmap

--- src/main/java/com/nexusforge/reactivekafka/sec10/KafkaConsumer.java
+++ src/main/java/com/nexusforge/reactivekafka/sec10/KafkaConsumer.java
@@ -20,7 +20,7 @@ public class KafkaConsumer {
 
     public static void main(String[] args) {
         var consumerConfig = Map.<String, Object>of(
-                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:8081",
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
