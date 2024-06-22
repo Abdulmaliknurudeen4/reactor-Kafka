97b89a67e2d087e1a0931fe79080db02226b55e4
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-23 00:10:37 +0100

    10 - Error Handling in Kafka
    Poison Pills Fixed with Custom Deserializers

--- src/main/java/com/nexusforge/reactivekafka/sec14/KafkaConsumer.java
+++ src/main/java/com/nexusforge/reactivekafka/sec14/KafkaConsumer.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 
@@ -15,11 +16,11 @@ public class KafkaConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         var consumerConfig = Map.<String, Object>of(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
+               // ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
                 ConsumerConfig.GROUP_ID_CONFIG, "demo-group124",
                 ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
                 ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1",
@@ -27,6 +28,7 @@ public class KafkaConsumer {
         );
 
         var options = ReceiverOptions.<String, Integer>create(consumerConfig)
+                .withValueDeserializer(errorHandlingDeserializer())
                 .subscription(List.of("order-events"));
 
         // uses - non deamon thread...
@@ -37,9 +39,14 @@ public class KafkaConsumer {
                 .subscribe();
     }
 
-    // if server shutsdown or network issues before acknowledgement whne
-    // event has been processed, it can lead to unwanted scenarios like multiple
-    // order charges in payment services
-
-    //Acknowledgement is like a bookmark
+    private static ErrorHandlingDeserializer<Integer> errorHandlingDeserializer() {
+        var deserializer = new ErrorHandlingDeserializer<>(new IntegerDeserializer());
+        deserializer.setFailedDeserializationFunction(
+                info -> {
+                    log.error("failed record {}", new String(info.getData()));
+                    return -10_000;
+                }
+        );
+        return deserializer;
+    }
 }
