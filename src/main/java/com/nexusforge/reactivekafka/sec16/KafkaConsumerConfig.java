8d35b8a117411cdfcd0a895c2eca334a2247d2f8
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-27 08:31:14 +0100

    12 - Kafka Spring Reactor
    Printing TypeID

--- src/main/java/com/nexusforge/reactivekafka/sec16/KafkaConsumerConfig.java
+++ src/main/java/com/nexusforge/reactivekafka/sec16/KafkaConsumerConfig.java
@@ -1,9 +1,11 @@
 package com.nexusforge.reactivekafka.sec16;
 
+import com.fasterxml.jackson.databind.deser.std.JsonNodeDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import reactor.kafka.receiver.ReceiverOptions;
 
 import java.util.List;
@@ -14,6 +16,7 @@ public class KafkaConsumerConfig {
     @Bean
     public ReceiverOptions<String, OrderEvent> receiverOptions(KafkaProperties kafkaProperties) {
         return ReceiverOptions.<String, OrderEvent>create(kafkaProperties.buildConsumerProperties(null))
+                .consumerProperty(JsonDeserializer.REMOVE_TYPE_INFO_HEADERS, "false")
                 .subscription(List.of("order-events"));
     }
 
