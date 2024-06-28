f9f87de1c3337046920ff8497fff27d1205da54d
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-06-28 01:54:00 +0100

    14 - Kafka Security
    producing and consuming with SSL truststore

--- src/main/java/com/nexusforge/reactivekafka/sec18/KafkaConsumer.java
+++ src/main/java/com/nexusforge/reactivekafka/sec18/KafkaConsumer.java
@@ -3,12 +3,14 @@ package com.nexusforge.reactivekafka.sec18;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
@@ -25,14 +27,16 @@ public class KafkaConsumer {
                 ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1",
                 ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false,
                 SaslConfigs.SASL_MECHANISM, "PLAIN",
-                CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT",
+                CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL",
                 SaslConfigs.SASL_JAAS_CONFIG, """
                             org.apache.kafka.common.security.plain.PlainLoginModule required
                             username="admin"
                             password="secret"
                             user_admin="secret"
                             user_client="client-secret";
-                    """
+                    """,
+                SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, Paths.get("src/main/resources/kafka.truststore.jks").toAbsolutePath().toString(),
+                SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "changeit"
         );
 
         var options = ReceiverOptions.create(consumerConfig)
--- src/main/java/com/nexusforge/reactivekafka/sec18/KafkaProducer.java
+++ src/main/java/com/nexusforge/reactivekafka/sec18/KafkaProducer.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 import reactor.kafka.sender.SenderRecord;
 
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Map;
 
@@ -25,14 +27,16 @@ public class KafkaProducer {
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
                 SaslConfigs.SASL_MECHANISM, "PLAIN",
-                CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT",
+                CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL",
                 SaslConfigs.SASL_JAAS_CONFIG, """
                             org.apache.kafka.common.security.plain.PlainLoginModule required
                             username="admin"
                             password="secret"
                             user_admin="secret"
                             user_client="client-secret";
-                    """
+                    """,
+                SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, Paths.get("src/main/resources/kafka.truststore.jks").toAbsolutePath().toString(),
+                SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "changeit"
         );
         var options = SenderOptions.<String, String>create(producerConfig);
 
